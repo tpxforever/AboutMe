0c3fcce1073f35be19a1e4cd9bb33a6c5352e8fe
tpxforever <thivyesh.sivasuriyamoorthy@student.manchester.ac.uk>
2024-11-07 01:16:24 +0000

    Added scroll text, changed starting screen

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("aboutme-button").addEventListener("click", function() {
-        const aboutMeSection = document.getElementById("aboutme-section");
-        aboutMeSection.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    });
-    document.getElementById("projects-button").addEventListener("click", function(){
-        const projectsSection = document.getElementById("projects-section");
+// Fade In Hello Text
+window.onload = function(){
+    document.getElementById("Hello").style.opacity = 1;
+    document.body.classList.add("remove-scrolling")
+    document.getElementById("down-arrow").style.opacity = 1;
+}
+let timeout = setTimeout(() => {
+    document.querySelector('Name').scrollIntoView();
+  }, 5000);
 
-        projectsSection.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
+// When website is reloaded, scroll set to top
 
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
+document.getElementById("down-arrow").addEventListener("click", function() {
+    document.body.classList.remove("remove-scrolling")
+    document.getElementsByClassName("Name")[0].scrollIntoView({
+        behavior: "smooth"
     });
 });
